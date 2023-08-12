12f7ce514436d4f6afd08396ac8e375e1452e79a
Jacob Barber <jacob.barber@disneystreaming.com>
2023-08-11 22:12:15 -0500

    capture json body with script instead of action

---
+++ .github/workflows/scripts/extract-issue-json-body.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Issue number passed as an argument
+ISSUE_NUMBER="$1"
+
+# Fetch issue body using GitHub CLI
+BODY_CONTENT=$(gh api "repos/${GITHUB_REPOSITORY}/issues/${ISSUE_NUMBER}" --auth="$GITHUB_TOKEN" -q '.body')
+
+EXTRACTED_JSON=$(echo "$BODY_CONTENT" | awk '/^```/{flag=1; next} /^```/{flag=0} flag')
+
+echo "$EXTRACTED_JSON" | jq empty
+
+# Check the exit status to determine if JSON is valid
+if [[ $? -eq 0 ]]; then
+    # Github actions nonsense
+    EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
+    echo "payload<<$EOF" >> $GITHUB_OUTPUT
+    echo "$EXTRACTED_JSON" >> $GITHUB_OUTPUT
+    echo "$EOF" >> $GITHUB_OUTPUT
+
+    exit 0
+else
+    exit 1
+fi
+
