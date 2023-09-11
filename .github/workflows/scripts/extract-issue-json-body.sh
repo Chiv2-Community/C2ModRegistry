2988095213f274272a27136267d5606bca763a18
Jacob Barber <Jacoby6000@gmail.com>
2023-09-11 09:48:37 -0500

    Update extract-issue-json-body.sh

--- .github/workflows/scripts/extract-issue-json-body.sh
+++ .github/workflows/scripts/extract-issue-json-body.sh
@@ -10,15 +10,12 @@ EXTRACTED_JSON=$(echo "$BODY_CONTENT" | awk '/^```/{flag=1; next} /^```/{flag=0}
 echo "$EXTRACTED_JSON" | jq empty
 
 # Check the exit status to determine if JSON is valid
-if [[ $? -eq 0 ]]; then
-    # Github actions nonsense
-    EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
-    echo "payload<<$EOF" >> $GITHUB_OUTPUT
-    echo "$EXTRACTED_JSON" >> $GITHUB_OUTPUT
-    echo "$EOF" >> $GITHUB_OUTPUT
-
-    exit 0
-else
-    exit 1
+if [[ -z "$EXTRACTED_JSON" ]]; then
+  exit 1
 fi
 
+# Github actions nonsense
+EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
+echo "payload<<$EOF" >> $GITHUB_OUTPUT
+echo "$EXTRACTED_JSON" >> $GITHUB_OUTPUT
+echo "$EOF" >> $GITHUB_OUTPUT
