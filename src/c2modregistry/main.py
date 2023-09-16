25e94fdae06c190f6398f73f88aadff8a7fd719d
Jacob Barber <jacoby6000@gmail.com>
2023-09-16 16:36:25 -0500

    redirects

--- src/c2modregistry/main.py
+++ src/c2modregistry/main.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
 import argparse
 from datetime import datetime
+import functools
 import json
 import os
-from typing import Callable, List, Optional, Tuple
+from typing import Callable, Dict, List, Optional, Tuple
 
 from c2modregistry import add_release_tag, initialize_repo
 from c2modregistry import Mod
-from c2modregistry import generate_package_list, repo_to_index_entry
+from c2modregistry import get_all_text_lines_in_directory, repo_to_index_entry, parse_redirects
 from c2modregistry.models import Dependency, Release, Repo
-from c2modregistry.package_list import load_package_list
+from c2modregistry.package_list import generate_package_list, load_package_list, load_redirects
 
 from semantic_version import SimpleSpec, Version
 
@@ -56,27 +57,41 @@ def main() -> None:
     args = argparser.parse_args()
 
     if args.command == "process-registry-updates":
-        process_registry_updates("./registry", "./package_db/mod_list_index.txt", args.dry_run)
+        process_registry_updates("registry", "package_db", args.dry_run)
     elif args.command == "init":
         [org, repoName] = args.repo_url.split("/")[-2:]
-        init([Repo(org, repoName)], args.dry_run)
+        redirects = load_redirects("package_db")
+        init([Repo(org, repoName)], redirects, args.dry_run)
     elif args.command == "add":
         [org, repoName] = args.repo_url.split("/")[-2:]
-        add_release(Repo(org, repoName), args.release_tag, args.dry_run)
+        redirects = load_redirects("package_db")
+        add_release(Repo(org, repoName), args.release_tag, redirects, args.dry_run)
     elif args.command == "remove":
         [org, repoName] = args.repo_url.split("/")[-2:]
-        remove_mods([Repo(org, repoName)], args.dry_run)
+        redirects = load_redirects("package_db")
+        remove_mods([Repo(org, repoName)], redirects, args.dry_run)
     else:
         logging.error("Unknown command.")
         exit(1)
 
-def process_registry_updates(registry_dir: str, mod_list_index_path: str, dry_run: bool) -> None:
+def process_registry_updates(registry_dir: str, package_db_path: str, dry_run: bool) -> None:
     # Get repo lines from the registry dir
+
+    new_redirect_lines = get_all_text_lines_in_directory(os.path.join(registry_dir, "redirects"))
+    new_redirects = parse_redirects(new_redirect_lines)
+    existing_redirect_lines = load_redirects(package_db_path)
+
+    logging.info(f"Loaded {len(new_redirects)} new redirect entries.")
+    logging.info(f"Found {len(existing_redirect_lines)} existing redirect entries.")
+
+    with open(package_db_path + os.path.sep + "redirects.txt", "w") as file:
+        file.write('\n'.join(new_redirect_lines))
+
     logging.info("Loading package list entries...")
     updated_index_entries = generate_package_list(registry_dir)
-    previous_index_entries = []
+    previous_index_entries = [] 
     try:
-        with open(mod_list_index_path, "r") as file:
+        with open(os.path.join(package_db_path, "mod_list_index.txt"), "r") as file:
             previous_index_entries = file.read().splitlines()
     except FileNotFoundError:
         pass
@@ -90,7 +105,7 @@ def process_registry_updates(registry_dir: str, mod_list_index_path: str, dry_ru
         try:
             split_entries = [entry.split("/") for entry in new_entries]
             repo_entries = [Repo(entry[0], entry[1]) for entry in split_entries]
-            init(repo_entries, dry_run)
+            init(repo_entries, new_redirects, dry_run)
         except Exception as e:
             # If we fail to initialize a repo, remove it from the package list
             logging.error(f"Failed to initialize repos {repo_entries}: {e}\n")
@@ -101,7 +116,7 @@ def process_registry_updates(registry_dir: str, mod_list_index_path: str, dry_ru
         try:
             split_entries = [entry.split("/") for entry in removed_entries]
             repo_entries = [Repo(entry[0], entry[1]) for entry in split_entries]
-            remove_mods(repo_entries, dry_run)
+            remove_mods(repo_entries, new_redirects, dry_run)
         except Exception as e:
             # If we fail to remove a repo, add it back to the package list
             print(f"Failed to remove repos {repo_entries}: {e}\n")
@@ -119,12 +134,12 @@ def process_registry_updates(registry_dir: str, mod_list_index_path: str, dry_ru
     if not os.path.exists(DEFAULT_PACKAGE_DB_DIR):
         os.makedirs(DEFAULT_PACKAGE_DB_DIR)
 
-    with open(mod_list_index_path, "w") as file:
+    with open(package_db_path + os.path.sep + "/mod_list_index.txt", "w") as file:
         file.write('\n'.join(updated_index_entries))
 
     logging.info("Package list built.")
 
-def init(repos: List[Repo], dry_run: bool) -> None:
+def init(repos: List[Repo], redirects: Dict[str, str], dry_run: bool) -> None:
     logging.info(f"Initializing {len(repos)} repos...")
     mods = [initialize_repo(repo) for repo in repos]
     filtered_mods = [mod for mod in mods if mod is not None]
@@ -133,7 +148,7 @@ def init(repos: List[Repo], dry_run: bool) -> None:
         logging.error("Failed to initialize some repos.")
         exit(1)
     
-    validate_package_db(DEFAULT_PACKAGE_DB_DIR, filtered_mods)
+    validate_package_db(DEFAULT_PACKAGE_DB_DIR, filtered_mods, redirects)
 
     if dry_run:
         logging.warning("Dry run; not writing to package dir.")
@@ -153,13 +168,13 @@ def init(repos: List[Repo], dry_run: bool) -> None:
 
     logging.info("Successfully initialized all repos.")
 
-def add_release(repo: Repo, release_tag: str, dry_run: bool) -> None:
+def add_release(repo: Repo, release_tag: str, redirects: Dict[str, str], dry_run: bool) -> None:
     logging.info(f"Loading mod metadata for {repo}...")
     mod = load_mod(repo, DEFAULT_PACKAGES_DIR)
 
     if mod is None:
         logging.info(f"Mod {repo} not initialized.")
-        init([repo], dry_run)
+        init([repo], redirects, dry_run)
 
         # No need to coninue. Initialization will get all releases.
         return
@@ -178,7 +193,7 @@ def add_release(repo: Repo, release_tag: str, dry_run: bool) -> None:
         exit(1)
         return
     
-    validate_package_db(DEFAULT_PACKAGE_DB_DIR, [updated_mod])
+    validate_package_db(DEFAULT_PACKAGE_DB_DIR, [updated_mod], redirects)
 
     if dry_run:
         logging.warning("Dry run; not writing to mod metadata.")
@@ -190,7 +205,7 @@ def add_release(repo: Repo, release_tag: str, dry_run: bool) -> None:
     
     logging.info(f"Successfully added release {release_tag} to repo {repo}.")
 
-def remove_mods(repo_list: List[Repo], dry_run: bool) -> None:
+def remove_mods(repo_list: List[Repo], redirects: Dict[str, str], dry_run: bool) -> None:
     logging.info(f"Removing {len(repo_list)} mods...")
 
     removed_mod_package_path_segements = [f"/{repo.org}/{repo.name}.json" for repo in repo_list]
@@ -203,7 +218,7 @@ def remove_mods(repo_list: List[Repo], dry_run: bool) -> None:
         return True
 
 
-    validate_package_db(DEFAULT_PACKAGE_DB_DIR, [], filter_func)
+    validate_package_db(DEFAULT_PACKAGE_DB_DIR, [], redirects, filter_func)
         
     if dry_run:
         logging.warning("Dry run; not writing to mod metadata.")
@@ -229,7 +244,7 @@ def load_mod(repo: Repo, package_dir: str) -> Optional[Mod]:
     except FileNotFoundError:
         return None
     
-def validate_package_db(package_dir: str, additional_mods: List[Mod], mod_path_filter: Callable[[str], bool] = lambda x: True) -> None:
+def validate_package_db(package_dir: str, additional_mods: List[Mod], redirects: Dict[str, str], mod_path_filter: Callable[[str], bool] = lambda x: True) -> None:
     logging.info("Validating package database...")
     packages = load_package_list(package_dir + "/mod_list_index.txt")
     mods: List[Mod] = additional_mods
@@ -258,7 +273,7 @@ def validate_package_db(package_dir: str, additional_mods: List[Mod], mod_path_f
     for mod in mods:
         for release in mod.releases:
             for dep in release.manifest.dependencies:
-                found_release = find_dependency(mods, dep)
+                found_release = find_dependency(mods, dep, redirects)
                 if found_release is None:
                     missing_deps.append((release, dep))
 
@@ -273,7 +288,7 @@ def validate_package_db(package_dir: str, additional_mods: List[Mod], mod_path_f
     
     logging.info("Package database is valid.")
                 
-def find_dependency(mods: List[Mod], dep: Dependency) -> Optional[Release]:
+def find_dependency(mods: List[Mod], dep: Dependency, redirects: Dict[str, str]) -> Optional[Release]:
     for mod in mods:
         for release in mod.releases:
             release_tag = release.tag
@@ -284,10 +299,19 @@ def find_dependency(mods: List[Mod], dep: Dependency) -> Optional[Release]:
             if dep_version.startswith("v"):
                 dep_version = dep_version[1:]
 
-            if release.manifest.repo_url == dep.repo_url and Version(release_tag) in SimpleSpec(dep_version):
+            resolved_manifest_url = resolve_redirects(redirects, release.manifest.repo_url)
+            resolved_dep_url = resolve_redirects(redirects, dep.repo_url)
+            if resolved_manifest_url == resolved_dep_url and Version(release_tag) in SimpleSpec(dep_version):
                 return release
 
     return None
 
+def resolve_redirects(redirects: Dict[str,str], repo: str) -> str:
+    if repo in redirects:
+        result = redirects[repo]
+        return resolve_redirects(redirects, result)
+    else:
+        return repo
+    
 if __name__ == "__main__":
     main()
--- src/c2modregistry/package_list.py
+++ src/c2modregistry/package_list.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 from dataclasses import dataclass, field
+import functools
 from typing import List, Optional, Dict
 from datetime import datetime
 import os
 
 def repo_to_index_entry(repo: str) -> str:
     """An index entry is just $org/$repoName, which happens to be the last 2 pieces of a github repo url. This converts repo urls to index entries."""
-    repo = repo.strip().rstrip("/")
-    return "/".join(repo.split("/")[-2:])
+    repo = repo.strip().rstrip(os.path.sep)
+    return os.path.sep.join(repo.split(os.path.sep)[-2:])
 
 def load_package_list(path: str) -> List[str]:
     """Loads the package list from a file."""
@@ -18,6 +19,10 @@ def load_package_list(path: str) -> List[str]:
         return []
 
 def generate_package_list(directory: str) -> List[str]:
+    """Gets all lines from all files, ignoring any empty lines or lines starting with #"""
+    return list(map(repo_to_index_entry, get_all_text_lines_in_directory(directory)))
+
+def get_all_text_lines_in_directory(directory: str) -> List[str]:
     """Gets all lines from all files, ignoring any empty lines or lines starting with #"""
     all_lines = []
 
@@ -32,6 +37,20 @@ def generate_package_list(directory: str) -> List[str]:
                     line = line.strip()
                     # Exclude lines that start with '#' or are empty
                     if not line.startswith('#') and line != "":
-                        all_lines.append(repo_to_index_entry(line))
+                        all_lines.append(line)
 
     return all_lines
+
+def load_redirects(package_db_dir: str) -> Dict[str, str]:
+    """Loads the redirects from the redirects file."""
+    redirects_path = os.path.join(package_db_dir, "redirects.txt")
+    try:
+        with open(redirects_path, 'r') as file:
+            return parse_redirects(file.read().splitlines())
+    except FileNotFoundError:
+        return {}
+
+def parse_redirects(redirect_lines: List[str]) -> Dict[str, str]:
+    redirect_lines = [line.strip().split(" -> ") for line in redirect_lines if line.strip() != ""]
+    print(redirect_lines)
+    return {line[0]: line[1] for line in redirect_lines}
